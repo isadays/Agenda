2e82f9b3e9a5eab8e0e913758d29515483e7b6a1
isadays <isah.pld@gmail.com>
2020-07-18 20:16:53 -0300

    First commit _schedule_

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Event
+
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_event', 'date_create')
+    list_filter = ('users','date_event',)
+
+
+admin.site.register(Event, EventAdmin)
